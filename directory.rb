abf6891b79975e27c9f68f71130aa3f46f2ace46
Hesh <heshamf@gmail.com>
2017-06-21 13:52:52 +0100

    Put students into an array

--- directory.rb
+++ directory.rb
@@ -1,18 +1,33 @@
 #assiging number of students to variable
 student_count = 11
-#first, print the list of students
+#put all students in an array
+students = [
+"Dr. Hannibal Lecter",
+ "Darth Vader",
+ "Nurse Ratched",
+ "Michael Corelone",
+ "Alex DeLarge",
+ "The Wicked Witch of the West",
+ "The Terminator",
+ "Freddie Krueger",
+ "The Joker",
+ "Joffery Baratheon",
+"Norman Bates"]
+
+#and then print them
+
 puts "The Students of Villains Academy"
 puts "_____________"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corelone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "The Terminator"
-puts "Freddie Krueger"
-puts "The Joker"
-puts "Joffery Baratheon"
-puts "Norman Bates"
+puts students[0]
+puts students[1]
+puts students[2]
+puts students[3]
+puts students[4]
+puts students[5]
+puts students[6]
+puts students[7]
+puts students[8]
+puts students[9]
+puts students[10]
 #finally, print total number of students
 puts "Overall, we have #{student_count} great students"
